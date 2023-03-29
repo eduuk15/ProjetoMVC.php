8a5cae6fa6b484e8219e67c22cc9017796ec2df9
Eduardo <eduardo@conectra.com.br>
2023-03-29 17:06:16 -0300

    AULA 1

---
+++ app/Controller/Pages/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use \App\Utils\View;
+use \App\Model\Entity\Organization;
+
+class Home extends Page {
+    public static function getHome() {
+        $obOrganization = new Organization;
+        $content = View::render('pages/home', [
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description,
+            'site' => $obOrganization->site
+        ]);
+        return parent::getPage('WDEV - Canal - HOME', $content);
+    }
+}
---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model\Entity;
+
+class Organization {
+    public $id = 1;
+    public $name = 'Canal WDEV';
+    public $site = 'https://youtube.com.br/wdevoficial';
+    public $description = 'Lorem ipsum dolor sit amet';
+}
